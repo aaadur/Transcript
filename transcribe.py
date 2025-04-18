d0bcc313a4b22d8b6ac23127c26dcfcf6c821d7a
aaadur <56845103+aaadur@users.noreply.github.com>
2025-04-18 11:10:24 +0200

    Update transcribe.py
    Suppression appel mistral AI

--- transcribe.py
+++ transcribe.py
@@ -2,7 +2,7 @@ import sys
 from moviepy import VideoFileClip
 import whisper
 import os
-from langchain_mistralai import ChatMistralAI
+#from langchain_mistralai import ChatMistralAI
 
 os.environ["MISTRAL_API_KEY"]="4S819Bz6jMYsoZHL4pF8ou9kj8bkZjTb"
 
@@ -21,13 +21,13 @@ def transcribe_audio_to_text(audio_path):
     result = model.transcribe(audio_path, fp16=False, language='french')
     return result['text']
 
-def generate_test_cases(transcription):
-    # Charger le modèle Mistral Instruct
-    llm = ChatMistralAI(model='mistral-large-latest', temperature=0)
-    
-    character = "ton profil pour cette conversation est celui d'un consultant expert dans la rédaction de test. Ton role est de rédiger un cas de test à partir d'une description d'un cas metier"
-    messages = [    ("system",character,    ),    ("human", transcription)]         
-    ai_msg = llm.invoke(messages)
+#def generate_test_cases(transcription):
+#    # Charger le modèle Mistral Instruct
+#    llm = ChatMistralAI(model='mistral-large-latest', temperature=0)
+#    
+#    character = "ton profil pour cette conversation est celui d'un consultant expert dans la rédaction de test. Ton role est de rédiger un cas de test à partir d'une description d'un cas metier"
+#    messages = [    ("system",character,    ),    ("human", transcription)]         
+#    ai_msg = llm.invoke(messages)
     
     return str(ai_msg.content)
 
@@ -39,6 +39,7 @@ if __name__ == "__main__":
 # Chemins des fichiers
 video_path = sys.argv[1]
 audio_path = sys.argv[2]+"extract.wav"
+audiototext_path  = sys.argv[2]+"audio.txt"
 txt_path = sys.argv[2]+"cdt.txt"
 
 # Extraire l'audio
@@ -47,9 +48,11 @@ extract_audio_from_video(video_path, audio_path)
 # Transcrire l'audio en texte
 transcription = transcribe_audio_to_text(audio_path)
 print("Transcription :", transcription)
+with open(audiototext_path, "w") as fichier:
+    fichier.write(transcription)
 
 # Générer des cas de test
-test_cases = generate_test_cases(transcription)
-print("Cas de test :", test_cases)
+# test_cases = generate_test_cases(transcription)
+# print("Cas de test :", test_cases)
 with open(txt_path, "w") as fichier:
     fichier.write(test_cases)
